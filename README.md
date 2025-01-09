61a9bd28f377ac5814a4a2266f3af99155423131
alex-tong-aff <tcyalexaff@gmail.com>
2025-01-10 07:09:03 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+"# inspireRV" 
+"# inspireRV" 
---
+++ obj/User/main.d
@@ -0,0 +1,25 @@
+User/main.o: ../User/main.c ../User/./ch32v003fun/ch32v003_i2c.h \
+ D:\Alex\ Tong\InspireLab\Projects\RISCV\code\inspirematrix\ test\InspireRV3\User/ch32v003fun/ch32v003fun.h \
+ D:\Alex\ Tong\InspireLab\Projects\RISCV\code\inspirematrix\ test\InspireRV3\User/funconfig.h \
+ ../User/./data/colors.h ../User/./data/buttons.h \
+ ../User/./ch32v003fun/driver.h \
+ ../User/./ch32v003fun/ch32v003_GPIO_branchless.h ../User/./data/music.h \
+ ../User/./ch32v003fun/ws2812b_simple.h
+
+../User/./ch32v003fun/ch32v003_i2c.h:
+
+D:\Alex\ Tong\InspireLab\Projects\RISCV\code\inspirematrix\ test\InspireRV3\User/ch32v003fun/ch32v003fun.h:
+
+D:\Alex\ Tong\InspireLab\Projects\RISCV\code\inspirematrix\ test\InspireRV3\User/funconfig.h:
+
+../User/./data/colors.h:
+
+../User/./data/buttons.h:
+
+../User/./ch32v003fun/driver.h:
+
+../User/./ch32v003fun/ch32v003_GPIO_branchless.h:
+
+../User/./data/music.h:
+
+../User/./ch32v003fun/ws2812b_simple.h:
